bac94159bc865fad6188702a364a69f9353ab703
Murat Sankaya <muratsankaya2025@u.northwestern.edu>
2024-02-11 14:07:54 -0600

    add dummy information

--- src/Components/Scanner.jsx
+++ src/Components/Scanner.jsx
@@ -4,6 +4,7 @@ const Scanner = () => {
     1 - Upload Image button
     2 - Upload it to Firebase (wait for Murat to complete the Login Page)
   */
+  /* After user uploads image, i.e. clicks on upload, we need to hard code translations and instructions*/
 
   return <div> This is the scanner component</div>;
 };
