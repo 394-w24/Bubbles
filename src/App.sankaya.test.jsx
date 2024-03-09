c25b4e5add87b89ab59291e1002a0ecbf0cabe35
Murat Sankaya <muratsankaya2025@u.northwestern.edu>
2024-03-08 20:13:09 -0600

    Change test name

--- src/App.sankaya.test.jsx
+++ src/App.sankaya.test.jsx
@@ -1,8 +1,8 @@
 import { describe, expect, test } from "vitest";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("counter tests", () => {
+describe("Login state test", () => {
   test("Testing login page display", () => {
     render(<App />);
     expect(screen.getByText("Sign In")).toBeDefined();
